7ec1d0e1303cb461f0b7914d0ad53b6dad2755e8
Abid Ali <abidali31570@gmail.com>
2025-07-26 15:26:14 +0500

    Initial commit of Community Help Platform (Nuxt + NestJS)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.{js,vue,ts}",
+    "./pages/**/*.{js,vue,ts}",
+    "./app.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
