dac1633be96ee27a53ab89c7d4ac016689a1c674
Abid Ali <abid.ali@arhamsoft.org>
2025-07-28 17:02:32 +0500

    added authentications

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, UserModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
